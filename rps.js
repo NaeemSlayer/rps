155b6f119dd5a4b37ae66e55cc8932775099738b
NaeemSlayer <140403182+NaeemSlayer@users.noreply.github.com>
2024-03-30 13:58:29 +0500

    added playGame func

--- rps.js
+++ rps.js
@@ -18,11 +18,44 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection === 'Rock') ||
         (playerSelection === 'scissors' && computerSelection === 'Paper')
     ) {
-        return 'You win! ${playerSelection} beats ${computerSelection}.'; 
+        return `You win! ${playerSelection} beats ${computerSelection}.`; 
     } else {
-        return 'You lose! ${computerSelection} beats ${playerSelection}.'; 
+        return `You lose! ${computerSelection} beats ${playerSelection}.`; 
     }
   }
   
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
 
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice (Rock, Paper, Scissors): ");
+        const computerSelection = getComputerChoice();
+
+        console.log(`Round ${i + 1  }: `);
+        console.log(`Player's choice: ${playerSelection}`);
+        console.log(`Computer's choice: ${computerSelection}`);
+
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        // update the scores
+        if (result.includes('Win')) {
+            playerScore++;
+        } else if (result.includes('Lose')) {
+            computerScore++;
+        }
+    }
+
+    // finding the winner
+    if (playerScore > computerScore) {
+        console.log("Congrats, You Won!!!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose :( Try again...");
+    } else {
+        console.log("It's a tie!!!`")
+    }
+}
   
+// runs the game
+playGame();
