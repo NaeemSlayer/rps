9c560e71e1d72840d2751ce8b8f16d8730299708
NaeemSlayer <140403182+NaeemSlayer@users.noreply.github.com>
2024-03-30 13:39:31 +0500

    edited getComputerChoice func, added playRound func

--- rps.js
+++ rps.js
@@ -1,7 +1,28 @@
 // function returns a random element from array
 function getComputerChoice() {
-    const choices = ['0', '1', '2'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+// function to play a single round
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    // check for tie
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    }
+    // check for player win conditions
+    if (
+        (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'Paper')
+    ) {
+        return 'You win! ${playerSelection} beats ${computerSelection}.'; 
+    } else {
+        return 'You lose! ${computerSelection} beats ${playerSelection}.'; 
+    }
+  }
+  
+
+  
